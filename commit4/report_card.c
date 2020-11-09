f332a1543fcb3022d45322bcdba3fe247c48471f
seok0928 <j_h_suk0928@naver.com>
2020-11-09 19:46:53 +0900

    commit1

--- commit4/report_card.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-void main(){
-	printf("This is Report Cards.c\n");
-	printf("what is the difference ? \n");
-	printf("Math: 90 \n");
-	printf("English: 100 \n");
-}
--- report_card.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-void main(){
-	printf("This is Report Cards.c\n");
-	printf("what is the difference ? \n");
-}
